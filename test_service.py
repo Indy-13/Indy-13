58dd80f17ff480a160db60e32883e7a0d8957972
Petr Tobias <ptobias@alteryx.com>
2022-12-24 14:53:23 +0100

    Test more imports

--- test_service.py
+++ test_service.py
@@ -2,6 +2,8 @@ import time
 import time
 import platform
 from imbox import Imbox  # pip install imbox
+from logger import Logger
+from photos_api_helper import PhotosAPIHelper
 
 while True:
     print("This is a test python file!")
