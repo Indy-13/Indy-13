fa8b61ce9863390e41600f0f71d7b534616c8b6e
Petr Tobias <ptobias@alteryx.com>
2022-12-24 12:45:52 +0100

    Fixed wrong service name

--- create_service.sh
+++ create_service.sh
@@ -4,6 +4,6 @@ sudo cp email_downloader_py.service /lib/systemd/system/
 
 sudo systemctl daemon-reload
 
-sudo systemctl enable test-py.service
+sudo systemctl enable email_downloader_py.service
 
-sudo systemctl start test-py.service
+sudo systemctl start email_downloader_py.service
