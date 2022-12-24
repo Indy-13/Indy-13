a13f5d12b1cffaa14e84ab767ef85f58ce49ff12
Petr Tobias <ptobias@alteryx.com>
2022-12-24 14:58:46 +0100

    Another test

--- test_service.py
+++ test_service.py
@@ -1,8 +1,16 @@
 import time
-import time
 import platform
 from imbox import Imbox  # pip install imbox
 from logger import Logger
+
+import os
+import requests
+
+import pickle
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+
 from photos_api_helper import PhotosAPIHelper
 
 CREDS_FILE = "client_secret_fotopasti_fotky.json"
@@ -18,7 +26,7 @@ else:
 
 logger = Logger(logger_config)
 
-api_helper = PhotosAPIHelper(CREDS_FILE, logger)
+# api_helper = PhotosAPIHelper(CREDS_FILE, logger)
 
 
 while True:
