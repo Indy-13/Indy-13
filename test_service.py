2f2d68cbd9d996103025d2405c946283bb9f8a59
Petr Tobias <ptobias@alteryx.com>
2022-12-24 14:55:14 +0100

    Test some instances

--- test_service.py
+++ test_service.py
@@ -5,6 +5,22 @@ from imbox import Imbox  # pip install imbox
 from logger import Logger
 from photos_api_helper import PhotosAPIHelper
 
+CREDS_FILE = "client_secret_fotopasti_fotky.json"
+
+current_patform = str(platform.system()).lower()
+
+if current_patform == "windows":
+    logger_config = {"LOG_FILE": r"c:\Temp\EmailDownloader.log"}
+elif current_patform == "linux":
+    logger_config = {"LOG_FILE": "/home/ubuntu/EmailDownloader/EmailDownloader.log"}
+else:
+    logger_config = {"LOG_FILE": "EmailDownloader.log"}
+
+logger = Logger(logger_config)
+
+api_helper = PhotosAPIHelper(CREDS_FILE, logger)
+
+
 while True:
     print("This is a test python file!")
     time.sleep(20)
